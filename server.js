82083a49871e14050e9e6661c40af9e0bb399b6b
buytheaway <160744262+buytheaway@users.noreply.github.com>
2025-02-12 01:01:27 +0500

    Atlas connection 0.891v

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(path.join(ROOT_DIR, 'frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({
